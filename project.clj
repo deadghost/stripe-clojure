2175aaf8b7bf538f497e02771a40f5d8a7c952b0
deadghost <kennyoliu@gmail.com>
2015-11-16 15:52:28 +0530

    Update deps.

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git"
         :url "https://github.com/prachetasp/stripe-clojure"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-		 [clj-http "1.0.1"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+		 [clj-http "2.0.0"]]
   :signing {:gpg-key "28F821EB"})
